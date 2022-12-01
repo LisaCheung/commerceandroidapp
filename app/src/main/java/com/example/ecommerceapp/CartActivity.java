94b847b1b826eae0f56149e993d6c26f980a7c6b
LisaCheung <cheung.lisa111@gmail.com>
2022-12-01 08:40:43 -0800

    cart update

--- app/src/main/java/com/example/ecommerceapp/CartActivity.java
+++ app/src/main/java/com/example/ecommerceapp/CartActivity.java
@@ -1,8 +1,11 @@
 package com.example.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +14,12 @@ import android.widget.Button;
 
 import com.example.ecommerceapp.cartitemslst.CartItemAdapter;
 import com.example.ecommerceapp.cartitemslst.CartItemModel;
+import com.example.ecommerceapp.database.ItemsDAO;
+import com.example.ecommerceapp.database.ItemsDB;
+import com.example.ecommerceapp.database.entities.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CartActivity extends AppCompatActivity {
     private Button checkoutButton;
@@ -20,29 +29,28 @@ public class CartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cart);
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         //temp data
-        //TODO use db data
-        CartItemModel[] cartItemLst = new CartItemModel[]{
-new CartItemModel("name1", 1.23, 0),
-                new CartItemModel("name2", 50.0, 0),
-                new CartItemModel("name3", 5.25, 0)
-        };
+        //TODO use cart lst data
+        ItemsDB itemsDatabase = Room.databaseBuilder(getApplicationContext(),
+                ItemsDB.class, "itemsDb").allowMainThreadQueries().build();
+        ItemsDAO itemsDAO = itemsDatabase.itemsDAO();
+        List<Item> allItemsListings = itemsDAO.getAll();
+        CartItemModel cartItemLst[] = new CartItemModel[allItemsListings.size()];
+        for(int i = 0; i < allItemsListings.size(); i++){
+            Item temp = allItemsListings.get(i);
+            cartItemLst[i] = new CartItemModel(temp.getName(), temp.getPrice(), 0);
+        }
+//        CartItemModel[] cartItemLst = new CartItemModel[]{
+//new CartItemModel("name1", 1.23, 0),
+//                new CartItemModel("name2", 50.0, 0),
+//                new CartItemModel("name3", 5.25, 0)
+//        };
+        ItemCount itemCount = new ViewModelProvider(this).get(ItemCount.class);
+        //TODO live data lst + cartitemadapter
+
         CartItemAdapter cartItemAdapter = new CartItemAdapter(cartItemLst);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(cartItemAdapter);
-        /*
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-
-        RecyclerViewModel[] lst = new RecyclerViewModel[]{
-            new RecyclerViewModel("text1", R.drawable.app1),
-                new RecyclerViewModel("text2", R.drawable.app1),
-                new RecyclerViewModel("text3", R.drawable.app1)
-        };
-        CustomAdapter customAdapter = new CustomAdapter(lst);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(customAdapter);
-         */
         checkoutButton = findViewById(R.id.checkoutButton);
         checkoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/ecommerceapp/cartitemslst/CartItemAdapter.java
+++ app/src/main/java/com/example/ecommerceapp/cartitemslst/CartItemAdapter.java
@@ -7,9 +7,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.ecommerceapp.CartActivity;
+import com.example.ecommerceapp.ItemCount;
 import com.example.ecommerceapp.R;
+import com.example.ecommerceapp.database.entities.Item;
+import com.google.android.material.button.MaterialButton;
 
 public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.ViewHolder>{
     private CartItemModel[] allItemsCart;
@@ -22,11 +28,15 @@ public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.ViewHo
         private TextView itemNameTextView;
         private TextView itemPriceTextView;
         private TextView itemQuantityTextView;
+        private MaterialButton addItem;
+        private MaterialButton removeItem;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             itemNameTextView = itemView.findViewById(R.id.cart_item_name);
             itemPriceTextView = itemView.findViewById(R.id.cart_item_price);
             itemQuantityTextView = itemView.findViewById(R.id.cart_item_quantity);
+            addItem = itemView.findViewById(R.id.inc_cart_item);
+            removeItem = itemView.findViewById(R.id.dec_cart_item);
         }
     }
     @NonNull
@@ -37,13 +47,42 @@ public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.ViewHo
         ViewHolder viewHolder = new ViewHolder(listItem);
         return viewHolder;
     }
+/*
+itemCount = new ViewModelProvider(this).get(ItemCount.class);
+        LiveData<Integer> itemCount2 = itemCount.getInitialCount();
+        itemQuantity.setText(String.valueOf(itemCount2.getValue()));
+        itemCount2.observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                itemQuantity.setText(String.valueOf(integer.intValue()));
+            }
+        });
 
+        increaseCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                itemCount.increaseCount();
+            }
+        });
+ */
     @Override
     public void onBindViewHolder(@NonNull CartItemAdapter.ViewHolder holder, int position) {
         final CartItemModel recyclerViewModel = allItemsCart[position];
         holder.itemNameTextView.setText(recyclerViewModel.getName());
         holder.itemPriceTextView.setText(String.valueOf(recyclerViewModel.getPrice()));
         holder.itemQuantityTextView.setText(String.valueOf(recyclerViewModel.getQuantity()));
+        holder.addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        holder.removeItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
