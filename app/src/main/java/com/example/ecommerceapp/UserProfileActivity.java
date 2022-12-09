1d9bae67aae2d29132403a18cdaaf5a36809f3f4
LisaCheung <cheung.lisa111@gmail.com>
2022-12-08 19:30:10 -0800

    add delete user acct

--- app/src/main/java/com/example/ecommerceapp/UserProfileActivity.java
+++ app/src/main/java/com/example/ecommerceapp/UserProfileActivity.java
@@ -5,6 +5,8 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,15 +16,17 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.ecommerceapp.database.UsersFirestore;
-import com.example.ecommerceapp.database.entities.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
 
 import java.util.Map;
 
@@ -104,6 +108,51 @@ public class UserProfileActivity extends AppCompatActivity {
         deleteAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //TODO: delete from firebase Auth
+                EditText deleteET = new EditText(getApplicationContext());
+                new AlertDialog.Builder(UserProfileActivity.this)
+                        .setIcon(R.drawable.ic_baseline_delete_24)
+                        .setTitle("Confirm Deletion")
+                        .setMessage("Enter password:")
+                        .setView(deleteET)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                AuthCredential credential = EmailAuthProvider
+                                        .getCredential(user.getEmail(), deleteET.getText().toString().trim());
+                                user.reauthenticate(credential)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                user.delete()
+                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    new UsersFirestore().deleteUserByName(displayName);
+                                                                    Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                                                                    startActivity(i);
+                                                                }
+                                                            }
+                                                        });
+
+                                            }
+                                        });
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                            }
+                        }).show();
+
+
 
             }
         });
