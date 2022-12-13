b96cfda1856d3fac973bd76847fe2e4e0d99da74
LisaCheung <cheung.lisa111@gmail.com>
2022-12-12 22:18:32 -0800

    appium test - login pg

--- commerceappium/src/test/java/AppiumTest.java
+++ commerceappium/src/test/java/AppiumTest.java
@@ -1,5 +1,7 @@
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -9,8 +11,23 @@ public class AppiumTest {
     @Test
     public void appiumTest1() throws MalformedURLException {
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("Copy_of_Pixel 6 Pro API 33-2");
-        options.setApp("C:\\Users\\cheun\\Desktop\\commerceappium\\src\\test\\resources\\commerce-app.apk");
+        options.setDeviceName("Pixel 6 Pro API 33-copy3");
+        options.setApp("C:\\Users\\cheun\\Desktop\\ecommerceandroidapp\\commerceappium\\src\\test\\resources\\commerce-app.apk");
         AndroidDriver androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        androidDriver.findElement(By.id("com.example.ecommerceapp:id/loginEmail")).click();
+        androidDriver.findElement(By.id("com.example.ecommerceapp:id/loginEmail")).sendKeys("janesmith24@email.com");
+        androidDriver.findElement(By.id("com.example.ecommerceapp:id/loginPassword")).click();
+        androidDriver.findElement(By.id("com.example.ecommerceapp:id/loginPassword")).sendKeys("janesmith24@email.com");
+        androidDriver.findElement(By.id("com.example.ecommerceapp:id/loginButton")).click();
+//        androidDriver.findElement(By.xpath("//android.widget.LinearLayout[@content-desc=\"Items\"]")).click();
+//        androidDriver.quit();
     }
 }
+/*
+{
+  "app": "C:\\Users\\cheun\\Desktop\\ecommerceandroidapp\\commerceappium\\src\\test\\resources\\commerce-app.apk",
+  "deviceName": "Pixel 6 Pro API 33-copy3",
+  "platformName": "android",
+  "automationName": "UIAutomator2"
+}
+ */
